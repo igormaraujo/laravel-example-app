3755812585c7e0533cb091c119c53b7eb6c76291
igormaraujo <igoraraujo@ufpa.br>
2021-11-30 22:35:38 -0300

    fix: add cancel buttom to company create/edit form

--- resources/views/company/create.blade.php
+++ resources/views/company/create.blade.php
@@ -8,7 +8,8 @@
     <div>
         <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
             <div class="mt-5 md:mt-0">
-                <form action="{{ $company->id ? route('company.update', $company->id) : route('company.store') }}" method="POST" enctype="multipart/form-data">
+                <form action="{{ $company->id ? route('company.update', $company->id) : route('company.store') }}"
+                    method="POST" enctype="multipart/form-data">
                     @method($company->id ? 'PUT' : 'POST')
                     @csrf
                     <div class="shadow sm:rounded-md sm:overflow-hidden">
@@ -21,8 +22,7 @@
                                     <div class="mt-1 flex rounded-md shadow-sm">
                                         <input type="text" name="name" id="name"
                                             class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300 @error('name') border-red-500 @enderror"
-                                            placeholder="Company Name"
-                                            value="{{old('name') ?? $company->name}}">
+                                            placeholder="Company Name" value="{{ old('name') ?? $company->name }}">
                                     </div>
                                     @error('name')
                                         <div class="text-red-500 text-sm">{{ $message }}</div>
@@ -37,8 +37,7 @@
                                     <div class="mt-1 flex rounded-md shadow-sm">
                                         <input type="text" name="email" id="email"
                                             class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300 @error('email') border-red-500 @enderror"
-                                            placeholder="Company Email"
-                                            value="{{old('email') ?? $company->email}}">
+                                            placeholder="Company Email" value="{{ old('email') ?? $company->email }}">
                                     </div>
                                     @error('email')
                                         <div class="text-red-500 text-sm">{{ $message }}</div>
@@ -58,7 +57,7 @@
                                         <input type="text" name="website" id="website"
                                             class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300 @error('website') border-red-500 @enderror"
                                             placeholder="www.example.com"
-                                            value="{{old('website') ?? $company->website ? str_replace($company->website, 'https://', '') : null}}">
+                                            value="{{ old('website') ?? $company->website ? str_replace($company->website, 'https://', '') : null }}">
                                     </div>
                                     @error('website')
                                         <div class="text-red-500 text-sm">{{ $message }}</div>
@@ -71,15 +70,18 @@
                                 </label>
                                 <div class="mt-1 flex items-center">
                                     <span class="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
-                                        <img id="image-preview" src="{{$company->logo ? asset('storage/' . $company->logo) : '#'}}" style="display: {{$company->logo ? 'block' : 'none'}}" />
-                                        <svg id="current-image" class="h-full w-full text-gray-300" fill="currentColor" style="display: {{$company->logo ? 'none' : 'block'}}"
+                                        <img id="image-preview"
+                                            src="{{ $company->logo ? asset('storage/' . $company->logo) : '#' }}"
+                                            style="display: {{ $company->logo ? 'block' : 'none' }}" />
+                                        <svg id="current-image" class="h-full w-full text-gray-300" fill="currentColor"
+                                            style="display: {{ $company->logo ? 'none' : 'block' }}"
                                             viewBox="0 0 24 24">
                                             <path
                                                 d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
                                         </svg>
                                     </span>
                                     <input type="file" id="selectedFile" accept="image/*" name="logo"
-                                        style="display: none;" onchange="onChangeImage(event)"/>
+                                        style="display: none;" onchange="onChangeImage(event)" />
                                     <button type="button" onclick="document.getElementById('selectedFile').click();"
                                         class="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 @error('logo') border-red-500 @enderror">
                                         Change
@@ -91,6 +93,10 @@
                             </div>
                         </div>
                         <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                            <a href="{{ route('dashboard') }}"
+                                    class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+                                    Cancel
+                            </a>
                             <button type="submit"
                                 class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                                 Save
